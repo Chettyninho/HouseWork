ead47b024cd96c142815505a560c4a8ec24ae821
Chettyninho <acedomarfil1a@gmail.com>
2023-10-26 13:37:53 +0200

    miguelAndroidStudio_1.0

---
+++ app/src/main/java/com/example/myapplicationandstudio/crearCasa.java
@@ -0,0 +1,14 @@
+package com.example.myapplicationandstudio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class crearCasa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_casa);
+    }
+}
