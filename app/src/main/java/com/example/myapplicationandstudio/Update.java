8dae4d3f0a59bf87fc5adaf7229c97ebcbdf9927
Chettyninho <acedomarfil1a@gmail.com>
2023-11-07 00:43:25 +0100

    Update Casi funcional. seSoluciona hoy

--- app/src/main/java/com/example/myapplicationandstudio/Update.java
+++ app/src/main/java/com/example/myapplicationandstudio/Update.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -47,13 +48,54 @@ public class Update extends AppCompatActivity {
         }
 
         cursor.close();
+
+        Button buttonSave = findViewById(R.id.buttonSave);
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                actualizarDatos(view);
+            }
+        });
+
     }
-    //@Override
-    //protected void onDestroy() {
-       // super.onDestroy();
-        //if (db != null) {
-           // db.close();
-        //}
+    public void actualizarDatos(View v){
+        //obtenemos el valor de los editText
+        EditText name_editText = findViewById(R.id.inputName);
+        String newName = name_editText.getText().toString();
+        EditText  surName_editText = findViewById(R.id.inputSurname);
+        String newSurname = surName_editText.getText().toString();
+        EditText email_editText = findViewById(R.id.inputEmail);
+        String newEmail = email_editText.getText().toString();
+        EditText userName_editText = findViewById(R.id.inputUserName);
+        String newUserName = userName_editText.getText().toString();
+        EditText Pasword_editText = findViewById(R.id.inputPassword);
+        String newPassword = Pasword_editText.getText().toString();
+
+//no va joder mecaguen la puta .
+
+        //consulta a la base de datos para insertar.
+        dataBase aux = new dataBase(Update.this);
+        SQLiteDatabase db = aux.getWritableDatabase();
+        //consulta para comparar con la bbdd
+        Cursor cursor = db.rawQuery("SELECT email FROM USERS", null);
+
+        if(cursor.moveToFirst())
+            do {
+                if (db != null && (!newName.isEmpty() || !newSurname.isEmpty() || !newEmail.isEmpty() || !newUserName.isEmpty() || !newPassword.isEmpty())) {
+                    String emailFromCursor = cursor.getString(1);
+                    String updateQuery = "UPDATE USERS" +
+                            " SET name = '" + newName + "'," +
+                            " surName = '" + newSurname + "'," +
+                            " email = '" + newEmail + "'," +
+                            " userName = '" + newUserName + "'," +
+                            " password = '" + newPassword + "'" +
+                            " WHERE email = '" + emailFromCursor + "';";
+                    db.execSQL(updateQuery);
+                }
+            } while (cursor.moveToNext());
+
+        }
+    }
 
 
         // Accede al botón por su ID
@@ -62,4 +104,4 @@ public class Update extends AppCompatActivity {
         // Define el comportamiento cuando se hace clic en el botón Guardar
         //que viene a ser enviar a la bbdd
 
-    }
+
