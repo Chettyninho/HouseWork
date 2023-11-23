72bb6b45f8b47b96f2cf06bd4e203ac46e2eb67a
Chettyninho <acedomarfil1a@gmail.com>
2023-11-23 21:25:53 +0100

    arregladod el tema de listar tareas, hay que añadir el checkbox con el estado del bool para marcarlas como realizadas

--- app/src/main/java/com/example/myapplicationandstudio/ShowTareas.java
+++ app/src/main/java/com/example/myapplicationandstudio/ShowTareas.java
@@ -36,8 +36,16 @@ public class ShowTareas extends AppCompatActivity {
                 final String descripcion = cursor.getString(2);
 
                 // Nuevo TextView para cada tarea
-                TextView taskTextView = new TextView(this);
-                applyTaskTextViewStyle(taskTextView); //aplicarEstilo al TextView
+                final TextView taskTextView = new TextView(this);
+                applyTaskTextViewStyle(taskTextView, nombre); // Aplicar estilo y texto al TextView
+
+                // Agrega un margen inferior para separación entre contenedores
+                LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) taskTextView.getLayoutParams();
+                params.setMargins(0, 0, 0, 16); // Establece el valor deseado para el margen inferior
+                taskTextView.setLayoutParams(params);
+
+                //taskTextView.setVisibility(View.VISIBLE);
+
                 // Combina el nombre y la descripción usando SpannableString
                // SpannableString spannableString = new SpannableString("Nombre: " + nombre );
 
@@ -48,33 +56,27 @@ public class ShowTareas extends AppCompatActivity {
                 //taskTextView.setText(spannableString);
 
                 // Crea un nuevo TextView para cada descripción
-                final TextView descTextView = new TextView(this);
-                applyDescTextViewStyle(descTextView);
-                descTextView.setText("Descripción: " + descripcion);
-                descTextView.setVisibility(View.GONE); // Establece inicialmente la visibilidad en GONE
+                //final TextView descTextView = new TextView(this);
+                //applyDescTextViewStyle(descTextView);
+                //descTextView.setText("Descripción: " + descripcion);
+                //descTextView.setVisibility(View.GONE); // Establece inicialmente la visibilidad en GONE
 
                 // Agrega el OnClickListener a taskTextView para alternar la visibilidad de descTextView
                 taskTextView.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        if (descTextView.getVisibility() == View.VISIBLE) {
-                            // Si la descripción está visible, la ocultamos
-                            descTextView.setVisibility(View.GONE);
-                            // Mostramos solo el nombre en taskTextView
-                            taskTextView.setText("Nombre: " + nombre);
+                        if (taskTextView.getText().equals(nombre)) {
+                            // Si el texto actual es el nombre, cambiar a nombre + descripción
+                            taskTextView.setText(nombre + "\nDescripción: " + descripcion);
                         } else {
-                            // Si la descripción no está visible, la mostramos
-                            descTextView.setVisibility(View.VISIBLE);
-                            // Mostramos el nombre y la descripción en taskTextView
-                            taskTextView.setText("Nombre: " + nombre + "\nDescripción: " + descripcion);
+                            // Si el texto actual ya incluye la descripción, cambiar a solo nombre
+                            taskTextView.setText(nombre);
                         }
                     }
                 });
 
-
                 // Agrega los TextView al LinearLayout
                 layout.addView(taskTextView);
-                layout.addView(descTextView);
 
             } while (cursor.moveToNext());
         }
@@ -85,7 +87,7 @@ public class ShowTareas extends AppCompatActivity {
     //prueba para estilo de TaskTextView
 
     // Métodos para aplicar estilos específicos a los TextViews
-    private void applyTaskTextViewStyle(TextView textView) {
+    private void applyTaskTextViewStyle(TextView textView, String text) {
         // Establece el color de texto negro
         textView.setTextColor(getResources().getColor(android.R.color.black));
 
@@ -95,26 +97,28 @@ public class ShowTareas extends AppCompatActivity {
         // Establece bordes naranjas y redondeados
         textView.setBackground(getRoundOrangeBorder());
 
+        // Establece dimensiones
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+
+        // Agrega un margen superior para separación entre contenedores
+        params.setMargins(4, 22, 4, 4); // Establece el valor deseado para el margen superior
+
+        textView.setLayoutParams(params);
+
+        // Establece el texto inicial al nombre de la tarea
+        textView.setText(text);
+
         // Otros estilos...
-        textView.setTextSize(22);
+        // Puedes ajustar el tamaño del texto para dar más importancia al nombre
+        textView.setTextSize(18); // Tamaño del texto del nombre
+
         //textView.setPadding(8,12,8,12);
     }
 
-    private void applyDescTextViewStyle(TextView textView) {
-        // Establece el color de texto negro
-        textView.setTextColor(getResources().getColor(android.R.color.black));
-
-        // Establece el fondo blanco
-        textView.setBackgroundColor(getResources().getColor(android.R.color.white));
-
-        // Establece bordes naranjas y redondeados
-        textView.setBackground(getRoundOrangeBorder());
-
-        // Otros estilos...
-        textView.setTextSize(16);
-    }
-
-    // Método para obtener un fondo con bordes naranjas y redondeados GRACIAS AL CHAT
+    // Método para obtener un fondo con bordes naranjas y redondeados
     private Drawable getRoundOrangeBorder() {
         GradientDrawable shape = new GradientDrawable();
         shape.setShape(GradientDrawable.RECTANGLE);
